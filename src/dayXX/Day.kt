4c5f99fe98aa15137ef7027920fd4a6e9405f818
Rimgar <rimgar.post@gmail.com>
2024-10-22 23:42:09 +0200

    Add template

---
+++ src/dayXX/Day.kt
@@ -0,0 +1,26 @@
+package dayXX
+
+import println
+import readInput
+
+private const val folder = "day"
+
+fun main() {
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("$folder/test")
+    check(part1(testInput) == 142)
+    check(part2(testInput) == 281)
+
+    val input = readInput("$folder/input")
+    part1(input).println()
+    part2(input).println()
+}
+
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
